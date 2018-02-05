dd8be485d534368ef8f2d45d7a334f7fb5b508eb
Mario Quintana <pyrros2000@gmail.com>
2018-02-05 14:01:28 -0800

    started random function

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
 'use strict';
+
+
 /* 
 Sales
+
 -The minimum number of customers per hour.
 -The maximum number of customers per hour.
 -The average number of cookies purchased per customer.*/
 
 const sales = {
+//properties
     Location: 'PDX Airport',
     minCust: '23',
     maxCust: '65',
     avgCookieSale: 6.3,
-
-
+//methods
+function getRandomInt(min, max) {
+    min = Math.ceil(3);
+    max = Math.floor(24);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
 };
 
 const sales2 = {
+//properties
     Location: 'Pioneer Square',
     minCust: '3',
     maxCust: '24',
     avgCookieSale: 1.2,
+//methods
 };
 
 const sales3 = {
+//porperties
     Location: 'Powell\'s',
     minCust: '11',
     maxCust: '38',
     avgCookieSale: 3.7,
+//methods
+
 };
 
 const sales4 = {
+//properties
     Location: 'St. John\'s',
     minCust: '20',
     maxCust: '38',
     avgCookieSale: 2.3,
+//methods
+
 };
 
 const sales5 = {
+//properties
     Location: 'Waterfront',
     minCust: '2',
     maxCust: '16',
     avgCookieSale: 4.6,
-
-}
+//methods
+};
