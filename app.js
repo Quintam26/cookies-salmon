77778e9f22519213e23eb8788ef458c9c0f75acd
Mario Quintana <pyrros2000@gmail.com>
2018-02-05 13:27:25 -0800

    started sales html

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 'use strict';
+/* 
+Sales
+-The minimum number of customers per hour.
+-The maximum number of customers per hour.
+-The average number of cookies purchased per customer.*/
+
+const sales = {
+    Location: 'PDX Airport',
+    minCust: '23',
+    maxCust: '65',
+    avgCookieSale: 6.3,
+
+
+};
+
+const sales2 = {
+    Location: 'Pioneer Square',
+    minCust: '3',
+    maxCust: '24',
+    avgCookieSale: 1.2,
+
+};
+
+const sales3 = {
+    L
+}
