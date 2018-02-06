3ee6364900d3456e1a26af5a439a3976e524e698
Mario Quintana <pyrros2000@gmail.com>
2018-02-06 13:43:44 -0800

    put Id on < section> to use as parent element in JS

--- object.js
+++ object.js
@@ -14,7 +14,7 @@ const sales = {
     maxCust: '65',
     avgCookieSale: '6.3',
     cookiesArray: [],
-    time: ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:','8pm:', 'Total'],
+    time: ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:','8pm:'],
     //methods
 
     getRandomCookies: function() {
@@ -28,14 +28,19 @@ const sales = {
         }
     },
 
-    totalCookies: function(){
+   /* cookiesPerDay: function(){
+        let i = 0;
+        for (i = 0; i < 15; i++){
 
+        }
 
-    }
+    }*/
 
+    
 };
 
 sales.getRandomCookies();
+sales.cookiesPerDay();
 
 
 const sales2 = {
